a978ad85a00efb5426f0378c9f4019aec1fb58b7
Jaycedam <contact@jordancortes.dev>
2025-01-18 16:55:35 -0300

    refactor: move files into modules

--- flake.nix
+++ flake.nix
@@ -1,78 +1,82 @@
 {
-    description = "Example Darwin system flake";
+  description = "Example Darwin system flake";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        nix-darwin.url = "github:LnL7/nix-darwin/master";
-        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-        home-manager.url = "github:nix-community/home-manager";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    outputs = {
-        self,
-        nix-darwin,
-        nixpkgs,
-        home-manager,
-        ...
-        } @ inputs: let
-            username = "jay";
-            configuration = {
-                pkgs,
-                lib,
-                config,
-                ...
-                }: {
-                    # List packages installed in system profile. To search by name, run:
-                    # $ nix-env -qaP | grep wget
-                    nixpkgs.config.allowUnfree = true;
+  outputs =
+    {
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      username = "jay";
+      configuration =
+        {
+          pkgs,
+          lib,
+          config,
+          ...
+        }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          nixpkgs.config.allowUnfree = true;
 
-                    environment.systemPackages =
-                        [
-                        ];
+          environment.systemPackages = [
+          ];
 
-                    users.users.jay = {
-                        name = username;
-                        home = "/Users/jay";
-                    };
+          users.users.jay = {
+            name = username;
+            home = "/Users/jay";
+          };
 
-                    # Auto upgrade nix package and the daemon service.
-                    services.nix-daemon.enable = true;
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
 
-                    # Necessary for using flakes on this system.
-                    nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-                    # Set Git commit hash for darwin-version.
-                    system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
-                    # Used for backwards compatibility, please read the changelog before changing.
-                    # $ darwin-rebuild changelog
-                    system.stateVersion = 5;
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
 
-                    system.defaults = {
-                        finder.FXPreferredViewStyle = "clmv";
-                    };
+          system.defaults = {
+            finder.FXPreferredViewStyle = "clmv";
+          };
 
-                    # The platform the configuration will be used on.
-                    nixpkgs.hostPlatform = "aarch64-darwin";
-                };
-        in {
-            # Build darwin flake using:
-            # $ darwin-rebuild build --flake .#simple
-            darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
-                modules = [
-                    configuration
-                    home-manager.darwinModules.home-manager
-                    {
-                        # `home-manager` config
-                        home-manager.useGlobalPkgs = true;
-                        home-manager.useUserPackages = true;
-                        home-manager.users.jay = import ./home.nix;
-                    }
-                ];
-            };
-
-            # Expose the package set, including overlays, for convenience.
-            # darwinPackages = self.darwinConfigurations."tsukuyomi".pkgs;
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
         };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            # `home-manager` config
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jay = import ./modules/home.nix;
+          }
+        ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      # darwinPackages = self.darwinConfigurations."tsukuyomi".pkgs;
+    };
 }
