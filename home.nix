5e4b5d48da1d47100145e44fd3cc275cb1edc7d9
Jaycedam <contact@jordancortes.dev>
2025-01-18 12:13:59 -0300

    feat: zsh and fzf options migrated

--- home.nix
+++ home.nix
@@ -81,9 +81,15 @@ in {
                                 append = true;
                         };
                         historySubstringSearch = {
+				enable = true;
                                 searchDownKey = [ "^P" ];
                                 searchUpKey = [ "^N" ];
                         };
+
+			# Extra options not available as modules
+			# 1. reduce delay when entering normal mode for vi mode in the shell,
+			# 2. fixes CTRL-C shortcut for fzf in non US layouts
+			initExtra = "KEYTIMEOUT=1\nbindkey 'ç' fzf-cd-widget";
                 };
 
 		starship = {
@@ -109,6 +115,48 @@ in {
                                 "separator"="#ff966c";
                                 "spinner"="#ff007c";
                         };
+
+			defaultOptions = [
+					"--bind 'ctrl-y:accept'"
+					"--highlight-line"
+					"--height 90%"
+					"--info=inline-right"
+					"--layout=reverse-list"
+					"--border=none"
+					"--color=bg+:#2d3f76"
+					"--color=border:#589ed7"
+					"--color=fg:#c8d3f5"
+					"--color=gutter:#1e2030"
+					"--color=header:#ff966c"
+					"--color=hl+:#65bcff"
+					"--color=hl:#65bcff"
+					"--color=info:#545c7e"
+					"--color=marker:#ff007c"
+					"--color=pointer:#ff007c"
+					"--color=prompt:#65bcff"
+					"--color=query:#c8d3f5:regular"
+					"--color=scrollbar:#589ed7"
+					"--color=separator:#ff966c"
+					"--color=spinner:#ff007c"
+			];
+
+# OPTION-T shortcut to change directory: exclude folders, preview with bat
+			fileWidgetOptions = [
+				"--walker-skip .git,node_modules,target,Library,Applications,Pictures,Music,.local,.cache,.Trash"
+					"--preview 'bat -n --color=always {}'"
+			];
+
+# OPTION-C shortcut to change directory: exclude folders
+			changeDirWidgetOptions = [
+				"--walker-skip .git,node_modules,target,Library,Applications,Pictures,Music,.local,.cache,.Trash"
+			];
+
+# CTRL-R shortcut to search command history: CTRL-Y to copy the command into clipboard using pbcopy
+			historyWidgetOptions = [
+				"--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
+					"--color header:bold"
+					"--header 'Press CTRL-Y to copy command into clipboard'"
+			];
 		};
 
 		bat = {
